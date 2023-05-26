16bd4483a0aa8f76bb3bd442fbd09214d0203d61
EnkiW <ytcahekyttpocto@gmail.com>
2023-05-26 21:42:37 +0300

    Information in the code!

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let hours = prompt("Введіть число в годинах:");
 hoursNumber = parseInt(hours);
 let seconds = hoursNumber * 3600;
-let result = seconds;    <!--Я не бачу жодних причин для внесення в змінну(можна робити і без цього рядка, просто в alert замість "result" написати "seconds")-->
+<!--Я не бачу жодної причини вводити його в змінну (ви можете обійтися без цього рядка нижче, просто напишіть "seconds" замість "result" у alert)-->
+let result = seconds;
 alert(hoursNumber + " годин(а) буде дорівнювати " + result + " секунд");
+
+            // Я маю на увазі так:
+// let hours = prompt("Введіть число в годинах:");
+// hoursNumber = parseInt(hours);
+// let seconds = hoursNumber * 3600;
+// alert(hoursNumber + " годин(а) буде дорівнювати " + seconds + " секунд");
